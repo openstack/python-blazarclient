66e9beca8bb36630898b70ca7df9e24f8fef2420
Jason Anderson <jasonanderson@uchicago.edu>
2020-01-28 10:21:26 -0600

    Don't fetch entire list when looking up by ID
    Blazar APIs support directly fetching a resource (whether host, floating
    ip, or lease) with its ID. However, the default logic for this includes
    a block that supports additionally looking up the resource by name (to
    allow looking up e.g., a lease by it's name.) This requires fetching the
    entire list of all resources and iterating over them until a match is
    found, which is very inefficient. Moreover, this branch would be taken
    even if the input was already a UUID.
    
    This commit changes that behavior so that if a UUID is provided, it is
    used to directly fetch the resource, which seems to be what the original
    intent was in the first place.
    
    Change-Id: I5b1ccf4abfe4aa9068bc011764cac4b3507ef697

--- blazarclient/tests/v1/shell_commands/test_leases.py
+++ blazarclient/tests/v1/shell_commands/test_leases.py
@@ -328,6 +328,7 @@ class ShowLeaseTestCase(tests.TestCase):
     def test_show_lease(self):
         show_lease, lease_manager = self.create_show_command()
         lease_manager.get.return_value = {'id': FIRST_LEASE}
+        mock.seal(lease_manager)
 
         args = argparse.Namespace(id=FIRST_LEASE)
         expected = [('id',), (FIRST_LEASE,)]
@@ -342,6 +343,7 @@ class ShowLeaseTestCase(tests.TestCase):
             {'id': SECOND_LEASE, 'name': 'second-lease'},
         ]
         lease_manager.get.return_value = {'id': SECOND_LEASE}
+        mock.seal(lease_manager)
 
         args = argparse.Namespace(id='second-lease')
         expected = [('id',), (SECOND_LEASE,)]
@@ -366,6 +368,7 @@ class DeleteLeaseTestCase(tests.TestCase):
     def test_delete_lease(self):
         delete_lease, lease_manager = self.create_delete_command()
         lease_manager.delete.return_value = None
+        mock.seal(lease_manager)
 
         args = argparse.Namespace(id=FIRST_LEASE)
         delete_lease.run(args)
@@ -379,6 +382,7 @@ class DeleteLeaseTestCase(tests.TestCase):
             {'id': SECOND_LEASE, 'name': 'second-lease'},
         ]
         lease_manager.delete.return_value = None
+        mock.seal(lease_manager)
 
         args = argparse.Namespace(id='second-lease')
         delete_lease.run(args)
