openstack/python-blazarclient

commit c9114b7eb82620db8d4bee7ce660e9d03670fd2d

Default client service type to reservation
Provide a default service type to avoid having to specify one,
this brings it inline with other openstack clients.

Change-Id: Idf423a87402894c131e6a57a86edc78ddcfd3ff6

blazarclient/client.py
@@ -19,7 +19,7 @@ from blazarclient import exception
 from blazarclient.i18n import _
 
 
-def Client(version=1, *args, **kwargs):
+def Client(version=1, service_type='reservation', *args, **kwargs):
     version_map = {
         '1': 'blazarclient.v1.client.Client',
         '1a0': 'blazarclient.v1.client.Client',

commit cfabd241dfb791870b201443ef6f97b3fc36b040

Fix up client init
Commit c9114b7eb82620db8d4bee7ce660e9d03670fd2d introduced a critical
bug where service_type is not being passed through to the client making
it fail.

Change-Id: I6c19199785109cacef6d8b06e08d926efc6d4890
Closes-Bug: #1942330

blazarclient/client.py
@@ -34,4 +34,6 @@ def Client(version=1, service_type='reservation', *args, **kwargs):
                 })
         raise exception.UnsupportedVersion(msg)
 
-    return importutils.import_object(client_path, *args, **kwargs)
+    return importutils.import_object(client_path,
+                                     service_type=service_type,
+                                     *args, **kwargs)

blazarclient/tests/test_client.py
@@ -32,12 +32,14 @@ class BaseClientTestCase(tests.TestCase):
     def test_with_v1(self):
         self.client.Client()
         self.import_obj.assert_called_once_with(
-            'blazarclient.v1.client.Client')
+            'blazarclient.v1.client.Client',
+            service_type='reservation')
 
     def test_with_v1a0(self):
         self.client.Client(version='1a0')
         self.import_obj.assert_called_once_with(
-            'blazarclient.v1.client.Client')
+            'blazarclient.v1.client.Client',
+            service_type='reservation')
 
     def test_with_wrong_vers(self):
         self.assertRaises(exception.UnsupportedVersion,